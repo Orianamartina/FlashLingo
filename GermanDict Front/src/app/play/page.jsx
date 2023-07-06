672d09097d8f48975abf8bbba5b5fbd3d50f9a80
Orianamartina <orianapettinelli04@gmail.com>
2023-07-06 20:33:49 -0300

    session stats

--- GermanDict Front/src/app/play/page.jsx
+++ GermanDict Front/src/app/play/page.jsx
@@ -3,15 +3,16 @@ import Cards from "./cards"
 import {useDispatch, useSelector} from "react-redux";
 import {useState } from "react";
 import {  checkCard, endSession} from "./gameplay";
-import { redirect } from 'next/navigation';
+import SessionStats from "./sessionStats";
 import axios from "axios";
 import { useRouter } from 'next/router';
+import { redirect } from "next/dist/server/api-utils";
 
 
 export default function Play(){
     const cards = useSelector(state => state.gameSession)
     const id = useSelector(state => state.sessionId)
-
+    const router = useRouter();
     const [cardsPlayed, setCardsPlayed] = useState([])
     const [index, setIndex] = useState(0)
     const [finished, setFinished] = useState(false)
@@ -42,19 +43,24 @@ export default function Play(){
             endCurrentSession()
         }
         setTranslation([])
-        console.log(cardsPlayed)
+
     }
     const [error, setError]=useState()
+    const [redCardsEnd, setRedCardsEnd] = useState()
+    const [yellowCardsEnd, setYellowCardsEnd] = useState()
+    const [greenCardsEnd, setGreenCardsEnd] = useState()
     const endCurrentSession =async() =>{
         try {
             setFinished(true)
             let sessionCards = endSession(cardsPlayed)
+            setGreenCardsEnd(sessionCards.green_cards)
+            setYellowCardsEnd(sessionCards.yellow_cards)
+            setRedCardsEnd(sessionCards.red_cards)
             const token = await axios.get(`http://127.0.0.1:8000/csrf_token/`, {withCredentials:true})
             const csrf = token.data.csrf_token
             const saveSession = await axios.post("http://localhost:3000/api/saveSession",{sessionId: id, body: sessionCards, token: csrf})
             if (saveSession.status === 200) {
-                const router = useRouter();
-                router.push('/dashboard');
+                redirect('/dashboard')
             }
         } catch (error) {
         
@@ -64,18 +70,16 @@ export default function Play(){
     return (
         <div>
             
-            {finished?(<div><h1>Saving results</h1> </div>):(
+            {finished?(<div><h1>Saving results</h1>
+                        <SessionStats yellow_cards={yellowCardsEnd} green_cards ={greenCardsEnd} red_cards={redCardsEnd}></SessionStats> </div>):(
                 <div>
                    
-                 <Cards card={cards[index]} handleClick={handleClick} next={nextWord} submitted={submitted} setSubmitted={setSubmitted}></Cards>
-                 {translation.length? translation.map(translation => <h1>{translation}</h1>): ""}
+                    <Cards card={cards[index]} handleClick={handleClick} next={nextWord} submitted={submitted} setSubmitted={setSubmitted}></Cards>
+                    {translation.length? translation.map(translation => {if(translation != null){<h1>{translation}</h1>}}): ""}
 
-            
                 </div>)}
            
             <h1>{error?error:""}</h1>
-
-             <button onClick={endCurrentSession}>endSession</button>
         </div>
         
     )
